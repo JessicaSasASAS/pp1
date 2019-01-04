446fd99b68b6799d34e75cf945b989d0e3bc5b29
Jessica Sas <jsas2@mail.greenriver.edu>
2019-01-04 13:37:50 -0500

    printArray() function

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
+<?php
+$numbers= [7,9,8,9,8,8,6];
+
+function printArray($array)
+{
+    echo implode($array, ",");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PP1</title>
+    <title>Pair Program 1</title>
 </head>
 <body>
     <h1>PP1</h1>
+
+    <?php
+    printArray($numbers);
+    ?>
 
 </body>
 </html>
